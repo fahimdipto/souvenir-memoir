473a5edf2f594f4d79b657ac0c78a1b70a022f29
fahimdipto <sultanparvez77.sp@gmail.com>
2021-09-28 22:26:32 +0600

    fix PORT error

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/post',postRoutes);
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.ATLAS_URL, {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>app.listen(PORT,() =>console.log(`Server Running on port ${PORT}`))
 ).catch((error)=> console.log(error.message));
